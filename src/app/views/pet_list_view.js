ecf27da01c604a4212a7f5514fad77bb49fcad66
Ann Dai <gofarann@gmail.com>
2017-06-12 13:22:03 -0700

    added init and render to pet list view object

--- src/app/views/pet_list_view.js
+++ src/app/views/pet_list_view.js
@@ -1,8 +1,28 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
 
 import PetView from './pet_view';
 
 var PetListView = Backbone.View.extend({
+  initialize: function(params) {
+    this.template = params.template;
+  },
+
+  render: function(){
+    this.$('.pet-list').empty();
+
+    var that = this;
+
+    this.model.each(function(pet){
+      var petView = new PetView({
+        model: pet,
+        template: that.template
+      });
+      that.$('.pet-list').append(petView.render().el);
+
+    });
+  }
 
 });
 
