1d78416b3ce70284475979a7148bea3e123cb0f4
Ann Dai <gofarann@gmail.com>
2017-06-12 13:34:17 -0700

    adding crumbs to trouble shoot why petlistview not init-ing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import _ from 'underscore';
 
+
 import PetList from 'app/collections/pet_list';
 import PetListView from 'app/views/pet_list_view';
 
@@ -9,12 +10,16 @@ myPetList.fetch();
 
 var myPetListView = new PetListView({
   model: myPetList,
-  template:_.template($('#pet-card-template').html()),
+  template:_.template($('.pet-card-template').html()),
   el: 'main'
 });
 
 
+
 $(document).ready(function() {
   myPetListView.render();
+  console.log(">>>>>crumb");
+  console.log(myPetListView);
+
 
 });
--- src/app/views/pet_view.js
+++ src/app/views/pet_view.js
@@ -1,6 +1,20 @@
 import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
+import Pet from '../models/pet.js';
+
 
 var PetView = Backbone.View.extend({
+  initialize: function(params){
+    this.template = params.template;
+    // this.$el.addClass("")
+  },
+
+  render: function(){
+    var compiledTemplate = this.template(this.model.toJSON());
+    this.$el.html(compiledTemplate);
+    return this;
+  }
 
 });
 
