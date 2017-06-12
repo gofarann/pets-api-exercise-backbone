5e8be7063ea85a98ef35edfe9df4170987efd43b
Ann Dai <gofarann@gmail.com>
2017-06-12 13:11:10 -0700

    initpetlist and petlist view vars in app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,16 @@ import _ from 'underscore';
 import PetList from 'app/collections/pet_list';
 import PetListView from 'app/views/pet_list_view';
 
-$(document).ready(function() {
+var myPetList = new PetList();
 
+var myPetListView = new PetListView({
+  model: myPetList,
+  template:_.template($('#pet-card-template').html()),
+  el: 'main'
+});
+
+
+$(document).ready(function() {
+  myPetListView.render();
 
 });
